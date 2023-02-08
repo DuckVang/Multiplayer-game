5a4874fd412f3b8c0d6d71f96d3335362dc2faf9
Nygen <nducminh002@gmail.com>
2023-02-08 21:52:05 +0100

    Add: projectiles array

--- Game/src/Game-Logic/Spells/SpellClass.ts
+++ Game/src/Game-Logic/Spells/SpellClass.ts
@@ -1,18 +1,24 @@
 import Body from "../../../../Engine/src/components/Physical-Body/Body"
 import Vector from "../../../../Engine/src/Math/Vector"
+import { Projectile } from "../../Game-Objects/Projectiles/BallProjectile"
 
 export class Spell{
 
 
-    type: Body
+    
+    projectiles: Projectile[]
+
     energyCost: number
     speed: number
+
     dir: Vector
+
     duration:number
     gap:number
+
+
     constructor(){
    
-        // this.type = new Body()
         this.energyCost =0
         this.speed =0
         this.dir = new Vector(0,0)
