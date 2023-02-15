630f3f422d3a2506c59f2cd54df4f99487f5f655
Nygen <nducminh002@gmail.com>
2023-02-15 23:09:22 +0100

    Add: Test-Ball

--- Game/src/Game-Objects/Test-Objects/Test-Ball.ts
+++ Game/src/Game-Objects/Test-Objects/Test-Ball.ts
@@ -1,25 +1,26 @@
 import * as Color from "color"
 import { DisplayObject, Graphics } from "pixi.js"
+import { Ball } from "../../../../Engine/src/components/Physical-Body/Ball"
 import { Box } from "../../../../Engine/src/components/Physical-Body/Box"
 import Vector from "../../../../Engine/src/Math/Vector"
-import { DrawBox } from "../../Render/Shapes"
+import { DrawBall, DrawBox } from "../../Render/Shapes"
 import { IGameObject } from "../IGameObject"
 
 
-export class TestBox extends Box  implements IGameObject{
+export class TestBall extends Ball implements IGameObject {
 
     motionTrail: boolean
     motionPos: Vector[]
 
     constructor() {
-        super(200, 200, 300, 300, 30, 2)
+        super(10, 10, 10, 10)
         this.graphics = new Graphics()
 
     }
     render() {
 
         this.graphics.clear()
-        this.graphics = DrawBox(this.graphics, this)
+        this.graphics = DrawBall(this.graphics, this, 1)
 
     }
 }
