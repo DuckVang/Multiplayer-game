4a1c54825c8a340778e77dac43a88dc5e1c9f463
Nygen <nducminh002@gmail.com>
2023-02-15 23:05:18 +0100

    Change: TestBox now implements Game object interface

--- Game/src/Game-Objects/Test-Objects/Test-Box.ts
+++ Game/src/Game-Objects/Test-Objects/Test-Box.ts
@@ -1,18 +1,20 @@
 import * as Color from "color"
 import { DisplayObject, Graphics } from "pixi.js"
 import { Box } from "../../../../Engine/src/components/Physical-Body/Box"
+import Vector from "../../../../Engine/src/Math/Vector"
 import { DrawBox } from "../../Render/Shapes"
+import { IGameObject } from "../IGameObject"
 
 
-export class TestBox extends Box {
+export class TestBox extends Box  implements IGameObject{
 
+    motionTrail: boolean
+    motionPos: Vector[]
 
     constructor() {
         super(200, 200, 300, 300, 30, 2)
-
         this.graphics = new Graphics()
-        // this.graphics.position.x = this.pos.x
-        // this.graphics.position.y = this.pos.y
+
 
 
     }
