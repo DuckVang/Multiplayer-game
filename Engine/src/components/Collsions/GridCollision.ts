f9c2025b351383166e3683b4e304c0d7952bbd0b
Nygen <nducminh002@gmail.com>
2023-02-18 16:14:53 +0100

    Add: Grid collision functions

---
+++ Engine/src/components/Collsions/GridCollision.ts
@@ -0,0 +1,49 @@
+import WORLD from "../../../../Game/src/World/GlobalWorld";
+import Engine from "../../Main";
+import COLLISIONS from "../Models/Collisions";
+import { CollisionData } from "./CollisionData";
+import { checkColl } from "./Detection";
+import Body from "../Physical-Body/Body";
+
+function FindCollisionsGrid() {
+    let grid = Engine.prototype.GRID
+
+    for (let x = 0; x < grid.width; x++) {
+
+
+        for (let y = 0; y < grid.height; y++) {
+
+            const currentCell = grid.getObjectFromCell(x, y)
+            for (let dx = -1; dx <= 1; dx++) {
+                for (let dy = -1; dy <= 1; dy++) {
+
+                    const otherCell = grid.getObjectsFromCell(x + dx, y + dy)
+                    CheckCellsColision(currentCell, otherCell)
+
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+}
+function CheckCellsColision(cell1: Body[], cell2: Body[]) {
+
+    cell1.forEach(obj1 => {
+
+        cell2.forEach(obj2 => {
+
+
+            if (obj1 != obj2) {
+                let bestSat = checkColl(obj1, obj2)
+                if (bestSat) {
+                    Engine.prototype.COLLISIONS.push(new CollisionData(obj1, obj2, bestSat.axis, bestSat.pen, bestSat.vertex, true, true))
+                }
+            }
+        });
+    });
+}
