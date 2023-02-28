8a506207d735bf936f9275ef09511ee2bb7a6550
Nygen <nducminh002@gmail.com>
2023-02-28 19:49:20 +0100

    Change config for client and server, game is now commented

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,12 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const nodeExternals = require('webpack-node-externals');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
-
+const {merge} = require('webpack-merge');
 const {
   NODE_ENV = 'production',
 } = process.env;
-
-module.exports = {
-
+let defaultConfig = {
   watch: NODE_ENV === 'development',
-  target: "node",
-  mode: "development",
-  entry: {
-    // demo: "./Demo/src/index.ts",
-    game: "./Game/src/index.ts",
-    client: "./Client/index.ts",
-    server: "./Server/index.ts"
-
-  },
   module: {
     rules: [
       {
@@ -26,45 +15,113 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-
       { test: /\.css$/, use: ["style-loader", "css-loader"], }
     ]
   },
-
-  plugins: [
- 
-    new HtmlWebpackPlugin({
-      filename: 'client.index.html',
-      template: "./Client/public/index.html",
-      chunks: ['client']
-    }),
-    new HtmlWebpackPlugin({
-      cache: false,
-      filename: 'game.index.html',
-      template: "./Game/public/index.html",
-      chunks: ['game']
-    }),
-    new WebpackShellPluginNext({
-      onBuildEnd: ['npm run run:dev']
-    })
-
-  ],
-  externals: [nodeExternals()],
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, './Game/public'),
-    },
-    port: 8080,
-    compress: true,
-    open: ["/game.index.html"], 
-  },
-
   resolve: {
     extensions: ['.tsx', '.ts', '.js', ".css"],
   },
+  
+
+    
+    
+  
+}
+
+
+const serverConfig =merge(defaultConfig,{
+  externals: [nodeExternals()],
+  entry:  "./Server/index.ts",
+  target: 'node',
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './Dist'),
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.bundle.js',
   },
-};
+  plugins:[  new WebpackShellPluginNext({
+    onBuildEnd: ['npm run run:dev']
+  })],
+  //…
+})
+
+const clientConfig = merge(defaultConfig, {
+  target: 'web', // <=== can be omitted as default is 'web'
+  
+  entry:  "./Client/index.ts",
+  
+  plugins:[new HtmlWebpackPlugin({
+    filename: 'client.index.html',
+    template: "./Client/public/index.html",
+   
+  })],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'client.bundle.js',
+  },
+  //…
+})
+
+module.exports = [clientConfig,serverConfig];
+
+
+// module.exports = {
+
+//   watch: NODE_ENV === 'development',
+//   target: "node",
+//   mode: "development",
+//   entry: {
+//     // demo: "./Demo/src/index.ts",
+//     game: "./Game/src/index.ts",
+//     client: "./Client/index.ts",
+//     server: "./Server/index.ts"
+
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/,
+//       },
+
+//       { test: /\.css$/, use: ["style-loader", "css-loader"], }
+//     ]
+//   },
+
+//   plugins: [
+ 
+//     new HtmlWebpackPlugin({
+//       filename: 'client.index.html',
+//       template: "./Client/public/index.html",
+//       chunks: ['client']
+//     }),
+//     new HtmlWebpackPlugin({
+//       cache: false,
+//       filename: 'game.index.html',
+//       template: "./Game/public/index.html",
+//       chunks: ['game']
+//     }),
+//     new WebpackShellPluginNext({
+//       onBuildEnd: ['npm run run:dev']
+//     })
+
+//   ],
+//   externals: [nodeExternals()],
+
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, './Game/public'),
+//     },
+//     port: 8080,
+//     compress: true,
+//     open: ["/game.index.html"], 
+//   },
+
+//   resolve: {
+//     extensions: ['.tsx', '.ts', '.js', ".css"],
+//   },
+  
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, './Dist'),
+//   },
+// };
