a56d650d229df3d4f412e73f2e84c9b0d811a29e
Nygen <nducminh002@gmail.com>
2023-03-02 13:29:19 +0100

    Add folder for socket communication

--- Client/Game/UserInputs.ts
+++ Client/Game/UserInputs.ts
@@ -1,59 +1,39 @@
 
-
-
-export function AddControl(body: Body) {
-
-    {
-
-        document.addEventListener('keydown', function (e: any) {
-
-
-            switch (e.keyCode) {
-                case 65:
-                    body.left = true
-                    break;
-
-                case 87:
-                    body.up = true
-                    break;
-
-                case 68:
-                    body.right = true
-                    break;
-
-                case 83:
-                    body.down = true
-                    break;
-
-                default:
-                    break;
-            }
-
-        });
-
-        document.addEventListener('keyup', function (e: any) {
-            switch (e.keyCode) {
-                case 65:
-                    body.left = false
-                    break;
-
-                case 87:
-                    body.up = false
-                    break;
-
-                case 68:
-                    body.right = false
-                    break;
-
-                case 83:
-                    body.down = false
-                    break;
-
-                default:
-                    break;
-            }
-
-
-        });
+const obj = WORLD.player
+let justPressed = false
+document.addEventListener('keydown', function(e){
+    if(e.code === "ArrowLeft"){
+        if(obj.left === false){
+            justPressed = true;
+        }
+        obj.left = true;
     }
-}
+    if(e.code === "ArrowUp"){
+        if(obj.up === false){
+            justPressed = true;
+        }
+        obj.up = true;
+    }
+    if(e.code === "ArrowRight"){
+        if(obj.right === false){
+            justPressed = true;
+        }
+        obj.right = true;
+    }
+    if(e.code === "ArrowDown"){
+        if(obj.down === false){
+            justPressed = true;
+        }
+        obj.down = true;
+    }
+    if(e.code === "Space"){
+        if(obj.action === false){
+            justPressed = true;
+        }
+        obj.action = true;
+    }
+    if (justPressed === true){
+        emitUserCommands(obj);
+        justPressed = false;
+    }
+});
--- Client/index.ts
+++ Client/index.ts
@@ -1,9 +1,4 @@
-import WORLD from "../Game/src/World/GlobalWorld";
-import { io } from "socket.io-client";
-import Vector from "../Engine/src/Math/Vector";
-import { TestBox } from "../Game/src/Game-Objects/Test-Objects/Test-Box";
-import { Player } from "../Game/src/Game-Objects/Player";
-const socket = io("http://localhost:4000");
+import socket from "./Client-Communication/socket"
 console.log("Pog client is alive")
 socket.on("serverToClient", (data) => {
 
@@ -19,13 +14,4 @@ socket.on("UpdatePlayer", (player) => {
 
 })
 
-let spawn = new Vector(WORLD.width / 2, WORLD.height / 2)
-const testBox = new TestBox(spawn.x + 100, spawn.y + 200)
-const player = new Player(spawn.x + 100, spawn.y + 100)
-socket.emit("newPlayer", player)
-player.m = 100
-const player2 = new Player(spawn.x, spawn.y)
-player2.m = 10
-WORLD.initUI()
-WORLD.SetPlayer(player)
-WORLD.Start()
+
