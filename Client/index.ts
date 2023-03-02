d851446915c02cbc89590c0bf0c3921e2bbb164b
Nygen <nducminh002@gmail.com>
2023-03-02 20:25:37 +0100

    WIP

--- Client/index.ts
+++ Client/index.ts
@@ -1,17 +1,7 @@
-import socket from "./Client-Communication/socket"
-console.log("Pog client is alive")
-socket.on("serverToClient", (data) => {
-
-
-    alert(data)
-    socket.emit("clientToServer", "Hello from " + socket.id)
-
-
-})
-socket.on("UpdatePlayer", (player) => {
+import client from "./Networking/socket"
+import WORLD from "../Game/src/World/GlobalWorld"
 
 
 
-})
 
 
