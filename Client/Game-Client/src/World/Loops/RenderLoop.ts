6684159b2f1bbdf8f49fc29aa1b13fb9c2855d90
Nygen <nducminh002@gmail.com>
2023-03-18 18:15:26 +0100

    Add: render functions

--- Client/Game-Client/src/World/Loops/RenderLoop.ts
+++ Client/Game-Client/src/World/Loops/RenderLoop.ts
@@ -1,6 +1,10 @@
 import CLIENT from "../../../../Networking/socket";
+import { Player } from "../../Player";
 import { DrawBall } from "../../Render/Shapes";
 import instance from "../GlobalWorld";
+import Body from "../../../../../Engine/src/components/Physical-Body/Body";
+import { Circle } from "pixi.js";
+import { Constants } from "../../../../../Shared/Constants";
 
 export function RenderLoop() {
     if (!CLIENT.gameUpdate) return
@@ -21,5 +25,31 @@ export function RenderLoop() {
     }
 
 
+
+
+
+}
+function RenderPlayers(players: Player[]) {
+
+
+    for (const key in players) {
+        DrawBall(instance.BACKGROUND, players[key])
+    }
+}
+function RenderObjects(objects: Body[]) {
+    for (const key in objects) {
+
+        let obj = objects[key].comp
+        switch (obj.type) {
+
+
+            case Constants.SHAPES.CIRCLE:
+                DrawBall(instance.BACKGROUND, obj)
+                break
+        }
+
+
+
+    }
 
 }
