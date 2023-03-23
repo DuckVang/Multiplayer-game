6b846670ee30dc87a14b8e67227860cae11cb4c3
Nygen <nducminh002@gmail.com>
2023-03-23 19:37:15 +0100

    Delete: sass dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,17 +16,6 @@ let defaultConfig = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
     ],
   },
   resolve: {
