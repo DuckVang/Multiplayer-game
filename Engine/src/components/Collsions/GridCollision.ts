f973dd5c370f6502d53f73e3cfceed1365ac2d22
Nygen <nducminh002@gmail.com>
2023-04-06 16:48:48 +0200

    Change: grid cells size and grid coll detection

--- Engine/src/components/Collsions/GridCollision.ts
+++ Engine/src/components/Collsions/GridCollision.ts
@@ -5,48 +5,60 @@ import { CollisionData } from "./CollisionData";
 import { checkColl, collisionHandlingCondition } from "./Detection";
 import Body from "../Physical-Body/Body";
 
+const checked: Body[] = [];
+
 export function FindCollisionsGrid() {
-    let grid = Engine.prototype.GRID
+  let grid = Engine.prototype.GRID;
+  checked.length = 0;
 
-    for (let x = 0; x < grid.width; x++) {
-        for (let y = 0; y < grid.height; y++) {
-
-            const currentCell = grid.getObjectsFromCell(x, y)
-            for (let dx = -1; dx <= 1; dx++) {
-                for (let dy = -1; dy <= 1; dy++) {
-
-                    const otherCell = grid.getObjectsFromCell(x + dx, y + dy)
-                    CheckCellsColision(currentCell, otherCell)
-
-                }
-
-            }
-
-
+  for (let x = 0; x < grid.width; x++) {
+    for (let y = 0; y < grid.height; y++) {
+      const currentCell = grid.getObjectsFromCell(x, y);
+      for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+          const otherCell = grid.getObjectsFromCell(x + dx, y + dy);
+          CheckCellsColision(currentCell, otherCell);
         }
+      }
     }
-
-
-
+  }
 }
 export function CheckCellsColision(cell1: Body[], cell2: Body[]) {
-
-    cell1.forEach(obj1 => {
-
-        cell2.forEach(obj2 => {
-
-            if (obj1 != obj2) {
-                let bestSat = checkColl(obj1, obj2);
-                if (bestSat) {
-                    if (collisionHandlingCondition(obj1, obj2)) {
-                        Engine.prototype.COLLISIONS.push(new CollisionData(obj1, obj2, bestSat.axis, bestSat.pen, bestSat.vertex, true, true));
-                    }
-                    else {
-                        Engine.prototype.COLLISIONS.push(new CollisionData(obj1, obj2, bestSat.axis, bestSat.pen, bestSat.vertex, false, false));
-                    }
-
-                }
+  cell1.forEach((obj1) => {
+    cell2.forEach((obj2) => {
+      if (obj1 != obj2) {
+        if (checked.includes(obj1) || checked.includes(obj2)) {
+          let bestSat = checkColl(obj1, obj2);
+          if (bestSat) {
+            checked.push(obj1, obj2);
+            if (collisionHandlingCondition(obj1, obj2)) {
+              Engine.prototype.COLLISIONS.push(
+                new CollisionData(
+                  obj1,
+                  obj2,
+                  bestSat.axis,
+                  bestSat.pen,
+                  bestSat.vertex,
+                  true,
+                  true
+                )
+              );
+            } else {
+              Engine.prototype.COLLISIONS.push(
+                new CollisionData(
+                  obj1,
+                  obj2,
+                  bestSat.axis,
+                  bestSat.pen,
+                  bestSat.vertex,
+                  false,
+                  false
+                )
+              );
             }
-        });
+          }
+        }
+      }
     });
+  });
 }
