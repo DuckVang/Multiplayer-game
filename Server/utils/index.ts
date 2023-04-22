c79300944c0e32d83a9a60843ae6ca419c99311c
Nygen <nducminh002@gmail.com>
2023-04-22 15:07:56 +0200

    Add: generics to utils function

--- Server/utils/index.ts
+++ Server/utils/index.ts
@@ -1,13 +1,16 @@
 import { forEach } from "../../webpack.config";
 
-export function returnObjectsOnlyWith(dict: any, ...property: any) {
+export function returnObjectsOnlyWith<TObj>(
+  dict: { [key: string]: TObj },
+  ...property: (keyof TObj)[]
+) {
   if (!dict) return;
   let playersComp = Object.keys(dict).reduce((result: any, key: string) => {
     let newObj: any = {};
-    property.forEach((p: any) => {
+    property.forEach((p) => {
       newObj[p] = dict[key][p];
     });
-    
+
     result[key] = newObj;
     return result;
   }, {});
